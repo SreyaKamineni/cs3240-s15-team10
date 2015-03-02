e2c82dfe34b0d41ca51e3cfd67c6c3f0813347ee
Allie Ivener <adi2hf@virginia.edu>
2015-03-02 15:11:01 -0500

    changed settings.py back to normal

--- djangoApp/djangoApp/settings.py
+++ djangoApp/djangoApp/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for djangoApp project. HELLO
+Django settings for djangoApp project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
